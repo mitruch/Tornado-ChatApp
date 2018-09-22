2f2301bbac0c155b8444709c4298001654ad255a
mitruch <kasiuniaczek11@interia.pl>
2018-09-22 23:13:41 +0200

    Render index.html

--- src/chatApp.py
+++ src/chatApp.py
@@ -4,7 +4,7 @@ import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello tornado chat app")
+        self.render("./templates/index.html")
 
 
 def make_app():
