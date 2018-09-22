d2cdc8c45249b216d97ca6b02fd1c1fbba10ab0c
mitruch <kasiuniaczek11@interia.pl>
2018-09-22 23:16:26 +0200

    Added path to the templates

--- src/chatApp.py
+++ src/chatApp.py
@@ -1,16 +1,21 @@
+import os.path
+
 import tornado.ioloop
 import tornado.web
 
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.render("./templates/index.html")
+        self.render("index.html")
 
 
 def make_app():
-    return tornado.web.Application([
-        (r"/", MainHandler),
-    ])
+    return tornado.web.Application(
+        [
+            (r"/", MainHandler)
+        ],
+        template_path=os.path.join(os.path.dirname(__file__), "templates"),
+    )
 
 
 if __name__ == "__main__":
